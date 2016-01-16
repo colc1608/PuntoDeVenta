4937bf3a10cbfb31081934e2ec9c8cbd2f23eab3
loopezcesar <cesar.1608@outlook.com>
2016-01-16 12:38:28 -0500

    agrege todas las interfaces

---
+++ SolucionPuntoDeVenta/interfaz/AreaInterfaz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//agregando librerias
+using dominio;
+
+namespace interfaz
+{
+    public interface AreaInterfaz
+    {
+        bool registrarArea(Area area);
+        bool actualizarArea(Area area);
+        bool eliminarArea(Area area);
+        List<Area> listaDeAreas();
+        List<Area> buscarArea(string cadena);
+    }
+}
